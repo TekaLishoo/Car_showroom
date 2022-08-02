8dcf790a6d0ae62c011ee29a19b36687be3fb316
TekaLishoo <teka.lishoo@gmail.com>
2022-08-02 14:37:35 +0300

    create models

--- car_showroom/settings.py
+++ car_showroom/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sell',
     'rest_framework',
+    'django_countries',
 ]
 
 MIDDLEWARE = [
--- sell/admin.py
+++ sell/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Supplier, SupplierSales, CarShowroom, CarShowroomSales, Buyer, Car, CarBrand, CarModel, BodyType, \
+    EngineType, DriveType
 
-# Register your models here.
+admin.site.register(Supplier)
+admin.site.register(SupplierSales)
+admin.site.register(CarShowroom)
+admin.site.register(CarShowroomSales)
+admin.site.register(Buyer)
+admin.site.register(Car)
+admin.site.register(CarBrand)
+admin.site.register(CarModel)
+admin.site.register(BodyType)
+admin.site.register(EngineType)
+admin.site.register(DriveType)
