3dd52028fd8b79aaa8ba50ca38108ae18ef2fb79
TekaLishoo <teka.lishoo@gmail.com>
2022-08-25 09:56:21 +0300

    add statistics

--- src/carshowroom/serializers.py
+++ src/carshowroom/serializers.py
@@ -6,6 +6,7 @@ from .models import (
     CarShowroomBuyers,
     CarShowroomSales,
 )
+from django.db.models import Sum, F, Count
 
 
 class CarShowroomSerializer(serializers.ModelSerializer):
@@ -37,3 +38,83 @@ class CarShowroomSalesSerializer(serializers.ModelSerializer):
     class Meta:
         model = CarShowroomSales
         fields = "__all__"
+
+
+class ShowroomNumberOfSellsSerializer(serializers.ModelSerializer):
+    number_of_sales = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CarShowroom
+        fields = ("number_of_sales",)
+
+    def get_number_of_sales(self, *args, **kwargs):
+        return CarSells.objects.filter(showroom=self.context['showroom_id']).values('showroom').annotate(Sum('amount'))
+
+
+class ShowroomProfitSerializer(serializers.ModelSerializer):
+    profit = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CarShowroom
+        fields = ("profit",)
+
+    def get_profit(self, *args, **kwargs):
+        return CarSells.objects.filter(showroom=self.context['showroom_id']).values('showroom').annotate(
+            profit_sum=Sum(F('amount')*F('price')))
+
+
+class ShowroomUniqueCustomersSerializer(serializers.ModelSerializer):
+    unique_customers = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CarShowroom
+        fields = ("unique_customers",)
+
+    def get_unique_customers(self, *args, **kwargs):
+        return CarSells.objects.filter(showroom=self.context['showroom_id'])\
+            .values('buyer__first_name', 'buyer__last_name').distinct()
+
+
+class ShowroomByLocationSerializer(serializers.ModelSerializer):
+    showrooms_location = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CarShowroom
+        fields = ("showrooms_location",)
+
+    def get_showrooms_location(self, *args, **kwargs):
+        return CarShowroom.objects.values('location').annotate(Count('id'))
+
+
+class ShowroomUniqueSuppliersSerializer(serializers.ModelSerializer):
+    unique_suppliers = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CarShowroom
+        fields = ("unique_suppliers",)
+
+    def get_unique_suppliers(self, *args, **kwargs):
+        return CarShowroomPresence.objects.filter(showroom=self.context['showroom_id'])\
+            .values('supplier__name').distinct()
+
+
+class CarsBoughtAmountSerializer(serializers.ModelSerializer):
+    amount_cars = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CarSells
+        fields = ("amount_cars",)
+
+    def get_amount_cars(self, *args, **kwargs):
+        return CarSells.objects.values('car').annotate(Sum('amount')).order_by('-amount__sum')
+
+
+class CustomerSpentMoneySerializer(serializers.ModelSerializer):
+    spent_money = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CarSells
+        fields = ("spent_money",)
+
+    def get_spent_money(self, *args, **kwargs):
+        return CarSells.objects.values('buyer').annotate(spent_sum=Sum(F('amount')*F('price'))).order_by('-spent_sum')
--- src/carshowroom/views.py
+++ src/carshowroom/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render  # noqa F401
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
 from .serializers import (
     CarShowroomSerializer,
     CarShowroomPresenceSerializer,
     CarSellsSerializer,
+    CarsBoughtAmountSerializer,
+    CustomerSpentMoneySerializer,
+    ShowroomNumberOfSellsSerializer,
+    ShowroomProfitSerializer,
+    ShowroomUniqueCustomersSerializer,
+    ShowroomByLocationSerializer,
     CarBuyersSerializer,
     CarShowroomSalesSerializer,
+    ShowroomUniqueSuppliersSerializer,
 )
 from .models import (
     CarShowroom,
@@ -22,6 +30,8 @@ from .filters import (
 )
 from django_filters import rest_framework as filters
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class CarShowroomViewSet(
@@ -36,6 +46,62 @@ class CarShowroomViewSet(
     filterset_class = CarShowroomFilter
     ordering_fields = ["name", "create_time", "update_time", "location", "balance"]
     search_fields = ["name", "wishes", "wish_cars__choice_car__car__model__model"]
+
+    @action(methods=['get'], detail=True, serializer_class=ShowroomNumberOfSellsSerializer)
+    def number_of_sells(self, request, pk=None):
+        """
+        <api/carshowrooms/<pk>/number_of_sells/
+        will return a sum of sells for this particular showroom
+        """
+        serializer = self.get_serializer(data=request.data, context={'showroom_id': pk})
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=True, serializer_class=ShowroomProfitSerializer)
+    def profit(self, request, pk=None):
+        """
+        <api/carshowrooms/<pk>/profit/
+        will return a total profit for this particular showroom
+        """
+        serializer = self.get_serializer(data=request.data, context={'showroom_id': pk})
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=True, serializer_class=ShowroomUniqueCustomersSerializer)
+    def unique_customers(self, request, pk=None):
+        """
+        <api/carshowrooms/<pk>/unique_customers/
+        will return all unique customers for this particular showroom
+        """
+        serializer = self.get_serializer(data=request.data, context={'showroom_id': pk})
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=False, serializer_class=ShowroomByLocationSerializer)
+    def by_locations(self, request, pk=None):
+        """
+        <api/carshowrooms/by_locations/
+        will return all distinct locations with amount of showrooms
+        situated there
+        """
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=True, serializer_class=ShowroomUniqueSuppliersSerializer)
+    def unique_suppliers(self, request, pk=None):
+        """
+        <api/carshowrooms/<pk>/unique_suppliers/
+        will return all unique suppliers this particular showroom was dealing with
+        """
+        serializer = self.get_serializer(data=request.data, context={'showroom_id': pk})
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
 
 
 class CarShowroomPresenceViewSet(
@@ -62,6 +128,29 @@ class CarSellsViewSet(
     queryset = CarSells.objects.all()
     serializer_class = CarSellsSerializer
 
+    @action(methods=['get'], detail=False, serializer_class=CarsBoughtAmountSerializer)
+    def total(self, request, pk=None):
+        """
+        <api/carsells/total/
+        will return all distinct car with amount of items that were bought
+        in descending order
+        """
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+
+    @action(methods=['get'], detail=False, serializer_class=CustomerSpentMoneySerializer)
+    def profit_customers(self, request, pk=None):
+        """
+        <api/carsells/profit_customers/
+        will return all distinct customers with total spent by them money amount
+        in descending order
+        """
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
 
 class CarBuyersViewSet(
     mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
